75cb742f6ed756a15b74159fd9474477ea22119c
nailton-vieira <nailtonvieira@gmail.com>
2024-07-15 15:21:47 -0300

    Envindo base do projeto ViaCEP ao repo

---
+++ src/main/java/com/nailton/viacep/controller/ClienteController.java
@@ -0,0 +1,12 @@
+package com.nailton.viacep.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastroclientes/clientes")
+public class ClienteController {
+
+
+    
+}
---
+++ src/main/java/com/nailton/viacep/models/Cliente.java
@@ -0,0 +1,11 @@
+package com.nailton.viacep.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente_tb")
+public class Cliente {
+
+    
+}
